15b434564e3e48909dc0535f916cbdadee17ba45
Coen Elferink <coen_elferink@hotmail.com>
2022-04-11 21:28:38 +0200

    Refactored Book constructor to Class

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let myLibrary = []
 
-function Book(index, title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
 
 const addBookToLibrary = book => myLibrary.push(book)
